ad0857a27caca8db7b2b47eeeb4b9f096fb7c0c9
David Radjai <dradjai@yahoo.com>
2023-04-04 16:09:35 -0400

    ADDED endpoint to tv library

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const PORT = 3000;
 
 const app = express();
 app.use(cors());
+// import CRUD Functions
+import { addNewTv, getAllTv } from './src/tvLibrary.js';
 app.use(express.json);
+
+
+app.get('/tvShows', getAllTv);
+
+app.post('/tvShows', addNewTv);
+
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`));
 
