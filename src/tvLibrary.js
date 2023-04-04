45f2b89981eb23d616284be8ae46d4a8f77e94ee
David Radjai <dradjai@yahoo.com>
2023-04-04 16:03:44 -0400

    UPDATE get function

--- src/tvLibrary.js
+++ src/tvLibrary.js
@@ -3,15 +3,18 @@ import  { db } from './dbConnect.js'
 // CREATE Add new Tv
 export function addNewTv (req, res) {
   const newTv = req.body;
-  db.collection('tvShows').add(newTv)
+  db.collection('tvShows')
+    .add(newTv)
     .then(doc => res.status(201).send(`New Tv Added: ${doc.id}`))
     .catch(err => res.status(500).send(err));
 }
 
 // GET get all TV shows
 export async function getAllTv(req, res) {
-  const collection = await db.collection('tvShows').get()
-  .catch(err => res.status(500).send(err))
+  const collection = await db.collection('tvShows')
+    .get()
+    .catch(err => res.status(500).send(err))
+  
   const tvShowList = collection.doc.map(show => ({...show.data(), id: show.id}))
 }
 
